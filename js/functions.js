c3c75b0dab5e7c4e4253a40c6f68904ebaeaffba
TLLRSK <112385550+TLLRSK@users.noreply.github.com>
2023-07-02 20:22:53 +0200

    initial commit

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,17 @@
-// Con Vite, no hace falta colocar la ruta al modulo, ya te permite importar lo que quieras
-// Tambien, al descargar el proyecto no estaba axios en la lista de dependencias del package.json
-// deberia guardartela automaticamente si has hecho npm install axios
 import axios from 'axios';
 
-// Guay esto de dejarte la url fuera.
-// Ya para nota, puedes configurar la url base de axios para luego no tener que repetirla
-// https://axios-http.com/docs/config_defaults
 const apiURL = 'https://api.quotable.io/random';
 
 //get data from api and build quote
 export const getApi = (quoteContent, quoteAuthor, quoteMain) => {
     axios.get(apiURL)
       .then(response => {
-        /* Esto podrias parametrizarlo.
-        Siempre que veas que el codigo es muy parecido en dos sitios, piensa en sacarlo a funcion
-        const getTemplate = (data, key) => `<p class="quoteapp__${key} xl">${data[key]}"</p>`
-
-        const contentTemplate = getTemplate(response.data, 'content');
-        const authorTemplate = getTemplate(response.data, 'author');
-        */
         const contentTemplate = `
           <p class="quoteapp__content xl">${response.data.content}"</p>
         `
         const authorTemplate = `
           <p class="quoteapp__author xl">- ${response.data.author}</p>
         `
-        // Esto no se lo que es en este archivo, tendrias que importartelo del otro o prepararlo parametrizado
         quoteContent.innerHTML = contentTemplate;
         quoteAuthor.innerHTML = authorTemplate
         quoteMain.classList.add('show');
@@ -45,6 +31,3 @@ export const newQuote = (quoteContent, quoteAuthor, quoteMain) => {
       },200);
     },500);
 }
-
-// puedes poner los exports arriba en las funciones, o combinar estos
-// export { getApi, newQuote };
